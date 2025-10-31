0d30d6ad3e8a2f856fe483c657b53093473a640f
Arkadiusz Chwedczuk <chwedczukarek@gmail.com>
2025-10-31 20:05:33 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./assets/Doto-VariableFont_ROND,wght.ttf";
+import { HomeScreen } from "./HomeScreen";
+
+function App() {
+  return <HomeScreen />;
+}
+
+export default App;
