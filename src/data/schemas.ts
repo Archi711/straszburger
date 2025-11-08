15a913a3b25695daefe8957f01162e60e9cdac56
Arkadiusz Chwedczuk <chwedczukarek@gmail.com>
2025-11-08 09:33:46 +0100

    fix: set proper root dir...

---
+++ src/data/schemas.ts
@@ -0,0 +1,45 @@
+import * as R from "remeda";
+import z from "zod";
+
+export const playerSchema = z.object({
+	id: z.cuid(),
+	name: z.string(),
+	isHead: z.boolean(),
+	roundsPlayed: z.number().positive(),
+	roundsScored: z.number().positive(),
+});
+export type Player = z.infer<typeof playerSchema>;
+
+export const teamSchema = z.object({
+	score: z.number().positive(),
+	players: z.array(playerSchema),
+});
+
+export type Team = z.infer<typeof teamSchema>;
+
+export const roundAnswerSchema = z.object({
+	answer: z.string(),
+	value: z.number().positive(),
+});
+
+export const roundDataSchema = z.object({
+	question: z.string(),
+	answers: z.array(roundAnswerSchema).refine(
+		(els) => {
+			return R.sumBy(els, (e) => e.value) === 100;
+		},
+		{ error: "Suma punktów != 100" },
+	),
+});
+
+export type RoundData = z.infer<typeof roundDataSchema>;
+
+export const gameStateSchema = z.object({
+	rounds: z.array(roundDataSchema),
+	currentRoundIndex: z.number().positive(),
+	currentPlayer: z.cuid(),
+	isFinal: z.boolean(),
+	blueTeam: teamSchema,
+	redTeam: teamSchema,
+});
+export type GameState = z.infer<typeof gameStateSchema>;
